22c3a9f641bdcd1b644f583f53821fd5c8d1e7f1
jee <jag918@nyu.edu>
2018-05-04 03:38:35 +0000

    automatically backed up by learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -6,22 +6,24 @@ def assign_variable(value)
   name=value
 end
 
-def argue
-  
+def argue(value)
+  value
 end
 
-def greeting
+def greeting(greeting, name)
 
 end
 
 def return_a_value
+  "Nice"
 
 end
 
 def last_evaluated_value
+  "expert"
 
 end
 
-def pizza_party
+def pizza_party(topping="cheese")
 
 end
